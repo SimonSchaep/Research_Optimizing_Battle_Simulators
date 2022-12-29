3811fb1b929b0c4a1a91eda95ceb6c1ba931a1d4
Simon Schaep <simon.schaep@gmail.com>
2022-12-29 14:16:50 +0100

    fix for crash with lower size grid

--- projects/Exam/BattleSimulator/Grid.h
+++ projects/Exam/BattleSimulator/Grid.h
@@ -17,7 +17,7 @@ public:
 
 	const std::vector<Cell*>& GetCells()const { return m_CellPointers; };
 
-	int GetCellId(int row, int col)const { return min(max( row, 0), m_Rows - 1) * m_Cols + min(max(col, 0), m_Cols); };
+	int GetCellId(int row, int col)const { return min(max( row, 0), m_Rows - 1) * m_Cols + min(max(col, 0), m_Cols - 1); };
 	int GetCellId(const Elite::Vector2& position)const { return min(max(GetCellId(int(position.y / m_CellSize), int(position.x / m_CellSize)), 0), (m_Rows * m_Cols) - 1); };
 
 	void GetRowCol(int cellId, int& row, int& col)const;
